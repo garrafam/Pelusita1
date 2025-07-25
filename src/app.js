f539b8a44153919d64a35528badc2c77e0a2ccac
Marcos Garrafa <garrafam2001@yahoo.com.ar>
2025-07-24 21:50:05 -0300

    loginrenderpru3

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const facturaRoutes = require('./routes/facturaRoutes');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 
 // --- INICIO DE LA DEPURACIÓN ---
